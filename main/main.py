34201b29f5b8f9e0d9ff5398761fe1ebdfac3ed0
nlam511 <nathanlam111@gmail.com>
2020-04-19 12:36:00 -0400

    olhc function

--- main/main.py
+++ main/main.py
@@ -1,12 +1,25 @@
 import utils
+import pandas as pd
+from test_data import test_data
+from typing import TypeVar, List
+PandasDataFrame = TypeVar('pd.core.frame.DataFrame')
+
 def main():
-    test = [
-        {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'},
-        {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'},
-        {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
-    ]
-    df = utils.def_list_to_pandas(test, drop_columns=['key2'])
+    df = utils.def_list_to_pandas(test_data)
+    sum = olhc_average(df, 4)
     print('done')
+
+
+def olhc_average(df: PandasDataFrame, value: float):
+    sum = df.open + df.close + df.high + df.low
+    sum_div = sum / value
+    return sum
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
