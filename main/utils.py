76473b0e42b7caa7e0ab2f63367599625f3bc339
nlam511 <nathanlam111@gmail.com>
2020-04-19 10:19:21 -0400

    updated docstring

--- main/utils.py
+++ main/utils.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 def def_list_to_pandas(data: List, drop_columns: List = None):
     """
-        The constructor for ComplexNumber class.
+    Function to convert a list of dictionaries into a pandas data frame
 
-        Parameters:
-            data (List): data represented as a list of dictionaries
-            drop_columns (List): Columns to drop, default=None
+    Parameters:
+        data (List): data represented as a list of dictionaries
+        drop_columns (List): Columns to drop, default=None
 
-        Return:
-            data formatted in a pandas data frame
+    Return:
+        data formatted in a pandas data frame
     """
     df = pd.DataFrame(data)
     return df.drop(drop_columns, axis=1)
