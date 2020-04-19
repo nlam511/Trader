9362f3d4a2b61e64b49a91bc94924e3404f8b904
aleto44 <alexjohn51015@yahoo.com>
2020-04-19 13:56:36 -0400

    ema method

--- main/candle_math.py
+++ main/candle_math.py
@@ -1,10 +1,38 @@
-from typing import List
+from typing import TypeVar, List
+PandasDataFrame = TypeVar('pd.core.frame.DataFrame')
+import pandas as pd
 
-def getSMA(prices: List):
-    sma = []
 
 
-def getEMA(prices: List):
+def get_sma(periods : int, price_data: PandasDataFrame):
+    """
+        SMA: simple moving average
+        =Sum(price,period)/ periods
+    :param price_data:
+    """
+    price_data.drop(price_data.columns[1], axis=1, inplace = True)
+    price_data['SMA'] = price_data.rolling(window=periods).mean()
+    return price_data
+
+#
+# int getSMA(int length, int arrayLength, double & prices[], double & returnArray[])
+# {
+#     int smaLength = arrayLength - length;
+# ArrayResize(returnArray, smaLength);
+#
+# for (int x = 0; x < smaLength; x++)
+# {
+#     returnArray[x] = 0.0;
+# for (int y = x; y < x + length; y++)
+# {
+#     returnArray[x] += prices[y];
+# }
+# returnArray[x] /= length;
+# }
+# return smaLength;
+# }
+
+def get_ema(prices: List):
     """
         EMA: exponential moving average;
         start with the SMA build from there.
